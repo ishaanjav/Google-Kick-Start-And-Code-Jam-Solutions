17694ff3fd2a6a0a3e3b3dfb8e7d99ffcf29c416
ishaanjav <ishaanjav@gmail.com>
2021-03-14 18:42:04 -0500

    Solution

---
+++ Kick Start/2020 E/p1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    int orig = t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int ar[n];
+        for (int i = 0; i < n; i++) cin >> ar[i];
+
+        int answer = 2;
+        int dif = ar[0] - ar[1];
+        int soFar = 2;
+        for (int i = 1; i < n - 1; i++) {
+            if (ar[i] - ar[i + 1] == dif) {
+                soFar++;
+                answer = max(answer, soFar);
+            } else {
+                dif = ar[i] - ar[i + 1];
+                soFar = 2;
+            }
+        }
+        cout << "Case #" << orig - t << ": " << answer << "\n";
+    }
+}
