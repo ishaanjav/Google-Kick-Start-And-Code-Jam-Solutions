7cd84be34f0896bd0d48371a793dc016cdc69a2f
ishaanjav <ishaanjav@gmail.com>
2021-03-28 11:52:50 -0500

    Small update

--- Code Jam/2021 Qualification/p1.cpp
+++ Code Jam/2021 Qualification/p1.cpp
@@ -1,3 +1,9 @@
+/*
+ * Created by ishaanjav
+ * github.com/ishaanjav
+ * Solutions: https://github.com/ishaanjav/Google-Kick-Start-And-Code-Jam-Solutions
+ */
+
 #include <iostream>
 using namespace std;
 
